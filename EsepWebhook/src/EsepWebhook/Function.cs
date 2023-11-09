14be0249aa0d02c058e8c0235ec76981fa0e75b9
Milena Petrovic <92889847+milenapetrov@users.noreply.github.com>
2023-11-09 17:29:33 +0100

    buggin

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -10,12 +10,13 @@ namespace EsepWebhook;
 public class Function
 {
     
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
+    // / <summary>
+    // / A simple function that takes a string and does a ToUpper
+    // / </summary>
+    // / <param name="input"></param>
+    // / <param name="context"></param>
+    // / <returns></returns>
+    
     public string FunctionHandler(string input, ILambdaContext context)
     {
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
